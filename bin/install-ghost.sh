310a91f4aa712a68bdefac882c440bd25ca9aceb
Mike Corkum <mcorkum@gmail.com>
2018-02-03 23:25:14 -0500

    fuck this

--- bin/install-ghost.sh
+++ bin/install-ghost.sh
@@ -3,9 +3,6 @@ mysql -e "create database IF NOT EXISTS $DB_NAME;" -uroot
 
 # Install Gulp CLI rebuild node sass
 npm install --global gulp-cli
-echo "here"
-npm rebuild node-sass
-
 
 # Download json parser for determining ngrok tunnel
 wget https://stedolan.github.io/jq/download/linux64/jq
